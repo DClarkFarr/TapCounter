d48945a207b162c906f09f04f4560074b17f0269
Daniel Farr <daniel@aptivada.com>
2022-10-02 20:46:59 -0600

    add cors

--- api/utils/app.ts
+++ api/utils/app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 export default app;
